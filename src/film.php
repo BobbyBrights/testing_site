36a045e9da9f22711110dbb540dc5fc0ad5f1235
John Armstrong <johnarmstrong@Johns-MacBook-Pro.local>
2017-04-17 18:38:18 -0400

    shit

--- src/film.php
+++ src/film.php
@@ -4,18 +4,25 @@ require 'src/getworkforpurchase.php';
 
 $purchase_json = getWorkForPurchaseJson($match['params']['film_id']);
 
-$film_json = getFilmJson($match['params']['film_id']);
-?>
-
-<link rel="stylesheet" type="text/css" href="<?=$web_host?>css/film.css">
-<script type="text/javascript">
-json_string = '<?php echo addslashes($film_json); ?>'
-if (json_string) {
-    film_obj = JSON.parse('<?php echo addslashes($film_json); ?>')
+if ($purchase_json) {
+    echo "";
 }
 else {
-    film_obj = '';
+    $film_json = getFilmJson($match['params']['film_id']);
+?>
+
+    <link rel="stylesheet" type="text/css" href="<?=$web_host?>css/film.css">
+    <script type="text/javascript">
+    json_string = '<?php echo addslashes($film_json); ?>'
+    if (json_string) {
+        film_obj = JSON.parse('<?php echo addslashes($film_json); ?>')
+    }
+    else {
+        film_obj = '';
+    }
+    </script>
+    <script src="<?=$web_host?>js/film.js" type="text/javascript"></script>
+    <script src="https://content.jwplatform.com/libraries/2d2HEAIU.js" type="text/javascript"></script>
+<?php 
 }
-</script>
-<script src="<?=$web_host?>js/film.js" type="text/javascript"></script>
-<script src="https://content.jwplatform.com/libraries/2d2HEAIU.js" type="text/javascript"></script>
+?>
