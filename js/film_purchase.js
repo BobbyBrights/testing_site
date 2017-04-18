633b75212d7688645efdb95b3a6023b750ecb521
John Armstrong <johnarmstrong@Johns-MacBook-Pro.local>
2017-04-18 12:30:27 -0400

    shit

--- js/film_purchase.js
+++ js/film_purchase.js
@@ -87,12 +87,12 @@ function getVolume(volumes) {
 
             //volume_film_details = film_details(volumes[i]['volumes_contents'][j])
             
-            alert(volume_film_details)
+            //alert(volume_film_details)
             alert(volumes[i]['volumes_contents'][j]['length'])
 
             if (volume_film_details) {
                 $(".film_text_container").last().append("<div class='" + getContainerClass(volumes[i]['link_colour']) + "_text'></div>")
-                $("." + getContainerClass(volumes[i]['link_colour']) + "_text").last().append(volume_film_details)
+                //$("." + getContainerClass(volumes[i]['link_colour']) + "_text").last().append(volume_film_details)
             }
         }
         $(".generic_table").last().append("<div class='generic_spacer'></div>")
