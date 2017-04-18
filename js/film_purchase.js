f7d86935bd513649ec704f491fd4df423e524ca1
John Armstrong <johnarmstrong@Johns-MacBook-Pro.local>
2017-04-18 11:36:15 -0400

    shit

--- js/film_purchase.js
+++ js/film_purchase.js
@@ -75,8 +75,11 @@ function getStill(film_object, img_container) {
     containerh = 188
     if (film_object['still_width'] < containerw || film_object['still_height'] < containerh) {
         $('.' + img_container).last().append("<img src='" + film_object['image_url'] + "' class='film_still_container_child'>")
-        $('.' + img_container).last().width($('.' + img_container + " > img").last().width())
-        $('.' + img_container).last().height($('.' + img_container + " > img").last().height())
+        $('.' + img_container + " > img").load(function() {
+            $('.' + img_container).last().width($('.' + img_container + " > img").last().width())
+            $('.' + img_container).last().height($('.' + img_container + " > img").last().height())
+        })
+        
     }
     else if (film_object['still_width'] == 740) {
         $('.' + img_container).last().append("<img src='" + film_object['image_url'] + "' class='film_still_container_child'>")
