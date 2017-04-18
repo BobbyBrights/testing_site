a869ff938f69db94301b3987cd503e9d08305cf2
John Armstrong <johnarmstrong@Johns-MacBook-Pro.local>
2017-04-17 20:44:51 -0400

    shit

--- js/film_purchase.js
+++ js/film_purchase.js
@@ -3,4 +3,14 @@ $(document).ready(function() {
     document.title = film_obj['film_title'] + " | Canadian Filmmakers Distribution Centre"
     $(".catalogue").css("color", "#E22134")
     $(".contents").append("<div class='accessible'></div>")
+    $(".accessible").last().append("<div class='purchase_table'></div>")
+    $(".purchase_table").last().append("<div class='purchase_table_cell_container'></div>")
+    
+    $(".purchase_table_cell_container").last().append("<div class='purchase_table_cell_left_margin'></div>")
+    $(".purchase_table_cell_container").last().append("<div class='purchase_table_contents'></div>")
+    $(".purchase_table_cell_container").last().append("<div class='purchase_table_cell_right_margin'></div>")
+
+    $(".purchase_table_contents").last().append("<div class='generic_table'></div>")
+    $(".generic_table").last().append("<div class='generic_table_title'><div class='generic_title'>"+ film_obj['film_title'] + "</div></div>")
+    $(".generic_table").last().append("<div class='generic_spacer'></div>")
 })
