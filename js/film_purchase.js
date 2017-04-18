3afaeb038c385ac8b997d915470d4768476b36b9
John Armstrong <johnarmstrong@Johns-MacBook-Pro.local>
2017-04-17 20:51:52 -0400

    shit

--- js/film_purchase.js
+++ js/film_purchase.js
@@ -13,4 +13,12 @@ $(document).ready(function() {
     $(".purchase_table_contents").last().append("<div class='generic_table'></div>")
     $(".generic_table").last().append("<div class='generic_table_title'><div class='generic_title'>"+ film_obj['film_title'] + "</div></div>")
     $(".generic_table").last().append("<div class='generic_spacer'></div>")
+
+    $(".generic_table").last().append("<div class='generic_info_table'></div>")
+
+    $(".generic_info_table").last().append("<div class='generic_info_about'>" + film_purchase_obj['film_description'] + "</div>")
+    $(".generic_info_table").last().append("<div class='generic_info_spacer'></div>")
+    $(".generic_info_table").last().append("<div class='generic_info_image_about'></div>")
+    $(".generic_info_image_about").last().append("<div class='img_container'></div>")
+    $(".img_container").last().append("<img src='" + film_purchase_obj['large_image_url'] + "' class='img_container_child'>")
 })
