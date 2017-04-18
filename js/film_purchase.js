dfeb7a9ab4fbc62b249f80a156e565826ed8e5a1
John Armstrong <johnarmstrong@Johns-MacBook-Pro.local>
2017-04-17 20:01:22 -0400

    shit

--- js/film_purchase.js
+++ js/film_purchase.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
     $(".contents").append("<div class='top_buffer'></div>")
-    document.title = film_obj['title'] + " | Canadian Filmmakers Distribution Centre"
+    document.title = film_obj['film_title'] + " | Canadian Filmmakers Distribution Centre"
     $(".catalogue").css("color", "#E22134")
     $(".contents").append("<div class='accessible'></div>")
 })
