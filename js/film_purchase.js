b0b4c9db8f85356a3d777dd9375198dd91d3c11a
John Armstrong <johnarmstrong@Johns-MacBook-Pro.local>
2017-04-18 13:24:02 -0400

    shit

--- js/film_purchase.js
+++ js/film_purchase.js
@@ -34,7 +34,7 @@ $(document).ready(function() {
         $(".generic_info_table").last().append("<div class='generic_info_landscape_purchase'><div class='generic_text_spacer_lg'></div>" + film_purchase_obj['film_description'] + "</div>")
     }
 
-    //$(".generic_table").last().append("<div class='generic_spacer'></div>")
+    $(".generic_table").last().append("<div class='generic_spacer'></div>")
 
     getVolume(film_purchase_obj['volumes'])
 })
