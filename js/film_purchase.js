d55fa573323b2b12f2600500c8bbec3af9da2fda
John Armstrong <johnarmstrong@Johns-MacBook-Pro.local>
2017-04-18 12:19:08 -0400

    shit

--- js/film_purchase.js
+++ js/film_purchase.js
@@ -85,6 +85,8 @@ function getVolume(volumes) {
 
             volume_film_details = film_details(volumes[i]['volumes_contents'][j])
 
+            alert(volume_film_details)
+
             if (volume_film_details) {
                 $(".film_text_container").last().append("<div class='" + getContainerClass(volumes[i]['link_colour']) + "_text'></div>")
                 $("." + getContainerClass(volumes[i]['link_colour']) + "_text").last().append(volume_film_details)
