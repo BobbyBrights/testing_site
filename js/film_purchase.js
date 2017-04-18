51b4e63126cb582b5a12138383a773fef34ea6c5
John Armstrong <johnarmstrong@Johns-MacBook-Pro.local>
2017-04-18 12:44:38 -0400

    shit

--- js/film_purchase.js
+++ js/film_purchase.js
@@ -120,7 +120,7 @@ function getStill(film_object, img_container) {
 }
 
 function film_details(film_obj) {
-    //film_detail = [film_obj['country'], film_obj['length'], film_obj['year'], film_obj['sound'], film_obj['colour'], film_obj['language']]
+    film_detail = [film_obj['country'], film_obj['length'], film_obj['year'], film_obj['sound'], film_obj['colour'], film_obj['language']]
     film_detail = []
     film_detail_str = "";
     film_detail_first_string_found = false;
