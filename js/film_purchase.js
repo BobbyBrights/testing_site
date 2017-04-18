cbabb6567513ff386c133ee115cb8a23b81a7289
John Armstrong <johnarmstrong@Johns-MacBook-Pro.local>
2017-04-18 13:37:08 -0400

    shit

--- js/film_purchase.js
+++ js/film_purchase.js
@@ -42,6 +42,10 @@ $(document).ready(function() {
 function getVolume(volumes) {
 
     for (i=0; i<volumes.length; i++) {
+        if (i != 0) {
+            $(".generic_table").last().append("<div class='generic_spacer'></div>")
+        }
+
         $(".generic_table").last().append("<div class='volume_container'></div>")
         $(".volume_container").last().append("<div class='volume_title_container'></div>")
         $(".volume_title_container").last().append("<div class='volume_title'><div class='" + getContainerClass(volumes[i]['link_colour']) + "'</div>" + volumes[i]['volume_title'] + "</div>")
@@ -116,7 +120,7 @@ function getVolume(volumes) {
             }
         }
 
-        $(".generic_table").last().append("<div class='generic_spacer'></div>")
+        
     }
 
     
