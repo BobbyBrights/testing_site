50a0f57c71f45269df66ded55b6f0045ed945717
John Armstrong <johnarmstrong@Johns-MacBook-Pro.local>
2017-05-01 16:58:31 -0400

    shit

--- js/toolbar.js
+++ js/toolbar.js
@@ -461,7 +461,7 @@ $( document ).ready(function() {
 
 	var currentDirectory = window.location.pathname.split('/').slice(0, -1).join('/')
 
-	alert(window.location.pathname)
+	//alert(window.location.pathname)
 
 	if (logged_in == 0) {
 		$(".bar").hide();
