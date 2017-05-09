25ff60e9447890a5b89f8d22d02e5e7a51966268
John Armstrong <johnarmstrong@Johns-MacBook-Pro.local>
2017-05-09 12:18:18 -0400

    shit

--- src/password_automation/getencrypted.php
+++ src/password_automation/getencrypted.php
@@ -1,6 +1,11 @@
 <?php
 require ('../../cms/pass_check.php');
-$input = $_GET["input"];
-$pass_func->hash($input);
-echo $pass_func;
+
+function generatePassword($length){
+    return substr(preg_replace("/[^a-zA-Z0-9]/", "", base64_encode(getRandomBytes($length+1))),0,$length);
+}
+
+$pass = generatePassword(8);
+$enc_pass = $pass_func->hash($pass);
+echo $enc_pass;
 ?>
