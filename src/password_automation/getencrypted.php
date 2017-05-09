a4514fb50fd0ab028fcda5002e26a7a88ff85d0f
John Armstrong <johnarmstrong@Johns-MacBook-Pro.local>
2017-05-09 12:19:05 -0400

    shit

--- src/password_automation/getencrypted.php
+++ src/password_automation/getencrypted.php
@@ -5,6 +5,14 @@ function generatePassword($length){
     return substr(preg_replace("/[^a-zA-Z0-9]/", "", base64_encode(getRandomBytes($length+1))),0,$length);
 }
 
+function getRandomBytes($nbBytes = 32)
+{
+    $bytes = openssl_random_pseudo_bytes($nbBytes, $strong);
+    if (false !== $bytes && true === $strong) {
+        return $bytes;
+    }
+}
+
 $pass = generatePassword(8);
 $enc_pass = $pass_func->hash($pass);
 echo $enc_pass;
