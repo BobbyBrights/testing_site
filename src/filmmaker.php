a3201e2e456ec877d486e762266ed4d2f04b5c8f
John Armstrong <johnarmstrong@Johns-MacBook-Pro.local>
2017-05-24 13:36:31 -0400

    shit

--- src/filmmaker.php
+++ src/filmmaker.php
@@ -14,3 +14,18 @@ else {
 }
 </script>
 <script src="<?=$web_host?>js/filmmaker.js" type="text/javascript"></script>
+
+<script>
+    if (filmmaker_obj) {
+        var el = document.createElement('script');
+        el.type = 'application/ld+json';
+        el.text = JSON.stringify({
+            "@context": "http://schema.org",
+            "@type": "WebPage",
+            "url": window.location.href,
+            "name": filmmaker_obj['filmmaker_name'] + " | Canadian Filmmakers Distribution Centre",
+            "keywords": filmmaker_obj['filmmaker_name'] + ", " + filmmaker_obj['filmmaker_name'] "CFMDC," + filmmaker_obj['filmmaker_name'] + "Canadian Filmmakers Distribution Centre"
+        });
+        document.querySelector('body').appendChild(el);
+    }
+</script>
--- src/news.php
+++ src/news.php
@@ -18,3 +18,15 @@ else {
 
 </script>
 <script src="<?=$web_host?>js/news.js" type="text/javascript"></script>
+
+<script>
+  var el = document.createElement('script');
+  el.type = 'application/ld+json';
+  el.text = JSON.stringify({
+    "@context": "http://schema.org",
+    "@type": "WebPage",
+    "url": window.location.href,
+    "name": "What's New | Canadian Filmmakers Distribution Centre"
+  });
+  document.querySelector('body').appendChild(el);
+</script>
