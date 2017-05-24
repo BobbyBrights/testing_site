84cf704ebb021d629ad65f590799c96f8abfbbd5
John Armstrong <johnarmstrong@Johns-MacBook-Pro.local>
2017-05-24 13:54:36 -0400

    shit

--- src/film.php
+++ src/film.php
@@ -52,14 +52,27 @@ else {
 
 <script>
     console.log(film_obj)
-    if (film_obj) {
+    if (film_obj && film_obj['secondary_filmmaker']) {
         var el = document.createElement('script');
         el.type = 'application/ld+json';
         el.text = JSON.stringify({
             "@context": "http://schema.org",
             "@type": "WebPage",
             "url": window.location.href,
-            "name": film_obj['film_title'] + " | Canadian Filmmakers Distribution Centre"
+            "name": film_obj['film_title'] + " | Canadian Filmmakers Distribution Centre",
+            "keywords": film_obj['secondary_filmmaker'] + ", " + film_obj['filmmaker_name'] + ", " + film_obj['film_title'] + ", " + "CFMDC,  Canadian Filmmakers Distribution Centre"
+        });
+        document.querySelector('body').appendChild(el);
+    }
+    elseif (film_obj) {
+        var el = document.createElement('script');
+        el.type = 'application/ld+json';
+        el.text = JSON.stringify({
+            "@context": "http://schema.org",
+            "@type": "WebPage",
+            "url": window.location.href,
+            "name": film_obj['film_title'] + " | Canadian Filmmakers Distribution Centre",
+            "keywords": film_obj['filmmaker_name'] + ", " + film_obj['film_title'] + ", " + "CFMDC,  Canadian Filmmakers Distribution Centre"
         });
         document.querySelector('body').appendChild(el);
     }
