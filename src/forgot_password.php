85454aba433f0e1ac945ec59015866b8f9693d6a
John Armstrong <johnarmstrong@Johns-MacBook-Pro.local>
2017-05-24 13:15:16 -0400

    shit

--- src/forgot_password.php
+++ src/forgot_password.php
@@ -2,3 +2,14 @@
 <script type="text/javascript">
 </script>
 <script src="<?=$web_host?>js/forgot_password.js" type="text/javascript"></script>
+<script>
+  var el = document.createElement('script');
+  el.type = 'application/ld+json';
+  el.text = JSON.stringify({
+    "@context": "http://schema.org",
+    "@type": "WebPage",
+    "url": window.location.href,
+    "name": "Forgot Password | Canadian Filmmakers Distribution Centre"
+  });
+  document.querySelector('body').appendChild(el);
+</script>
--- src/request_account.php
+++ src/request_account.php
@@ -2,3 +2,14 @@
 <script type="text/javascript">
 </script>
 <script src="<?=$web_host?>js/request_account.js" type="text/javascript"></script>
+<script>
+  var el = document.createElement('script');
+  el.type = 'application/ld+json';
+  el.text = JSON.stringify({
+    "@context": "http://schema.org",
+    "@type": "WebPage",
+    "url": window.location.href,
+    "name": "Request Account | Canadian Filmmakers Distribution Centre"
+  });
+  document.querySelector('body').appendChild(el);
+</script>
