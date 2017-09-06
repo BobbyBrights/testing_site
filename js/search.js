53d8da4b5eb32773d13d04a4fb25e77db9172ba0
John Armstrong <j2armstrong@gmail.com>
2017-09-06 16:30:38 -0400

    search

--- js/search.js
+++ js/search.js
@@ -160,7 +160,7 @@ function writeRecords(records) {
             else {
                 $(".search_table_content").append("<div class='generic_spacer'></div>")
             }
-            $(".search_table_content").append("<div class='genre_cat_heading'>Keywords</div>")
+            $(".search_table_content").append("<div class='genre_cat_heading'>Keywords <div class='genre_menu_link'><span class='glyphicon glyphicon-plus'></span></div></div>")
             $(".search_table_content").append("<div class='genre_cat_table'></div>")
             $(".genre_cat_table").last().append(getGenreCategories(records[i]['category']))
         }
@@ -173,7 +173,7 @@ function writeRecords(records) {
             else {
                 $(".search_table_content").append("<div class='generic_spacer'></div>")
             }
-            $(".search_table_content").append("<div class='genre_cat_heading'>Exhibition Format</div>")
+            $(".search_table_content").append("<div class='genre_cat_heading'>Exhibition Format <div class='genre_menu_link'><span class='glyphicon glyphicon-plus'></span></div></div>")
             $(".search_table_content").append("<div class='genre_cat_table'></div>")
             $(".genre_cat_table").last().append(getExhibition(records[i]['exhibition_format']))
         }
