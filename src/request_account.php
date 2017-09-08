5e42667d6ef5683ab2e172d26fbae69c93721a90
John Armstrong <j2armstrong@gmail.com>
2017-09-08 14:38:32 -0400

    filmmaker fix

--- src/request_account.php
+++ src/request_account.php
@@ -1,4 +1,4 @@
-<link rel="stylesheet" type="text/css" href="<?=$web_host?>css/request_account.css">
+<link rel="stylesheet" type="text/css" href="<?=$web_host?>css/film_submission.css">
 <script type="text/javascript">
 </script>
 <script src="<?=$web_host?>js/request_account.js" type="text/javascript"></script>
