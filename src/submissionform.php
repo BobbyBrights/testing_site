0cee790545beb1520528599ee17f73c16ef5f96b
John Armstrong <j2armstrong@gmail.com>
2017-09-08 13:51:48 -0400

    filmmaker fix

---
+++ src/submissionform.php
@@ -0,0 +1,18 @@
+<?php
+?>
+
+<link rel="stylesheet" type="text/css" href="<?=$web_host?>css/page.css">
+
+<!-- <script src="<?=$web_host?>js/studyguides.js" type="text/javascript"></script> -->
+
+<script>
+  var el = document.createElement('script');
+  el.type = 'application/ld+json';
+  el.text = JSON.stringify({
+    "@context": "http://schema.org",
+    "@type": "WebPage",
+    "url": window.location.href,
+    "name": "Submission Form | Canadian Filmmakers Distribution Centre"
+  });
+  document.querySelector('body').appendChild(el);
+</script>
