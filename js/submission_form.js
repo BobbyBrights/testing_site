399314300ad4bf7ecb3e6bc94fa314a3b0cf882e
John Armstrong <j2armstrong@gmail.com>
2017-09-11 15:02:49 -0400

    film form

--- js/submission_form.js
+++ js/submission_form.js
@@ -191,7 +191,7 @@ $(document).ready(function() {
 
     // recaptcha
     $(".table_contents").append("<div class='g-recaptcha-outer'><div class='g-recaptcha' data-callback='recaptchaCallback' data-sitekey='6LemsxcUAAAAACG6GzNABCispOz4O0qQFVCn4MT3'></div></div>")
-    $(".table_contents").append("<div class='spacer'></div>")
+    $(".table_contents").append("<div class='spacer_with_bar'></div>")
 
     $(".table_contents").append("<div class='note_table'></div>")
     $(".note_table").last().append("<div class='note_left_spacer'></div>")
@@ -203,7 +203,7 @@ $(document).ready(function() {
     $(".note_content_table").last().append("<div class='note_content_horz_spacer'></div>")
     $(".note_table").last().append("<div class='note_right_spacer'></div>")
 
-    $(".table_contents").append("<div class='spacer_with_bar'></div>")
+    $(".table_contents").append("<div class='spacer'></div>")
 
     // Button
     $(".table_contents").append("<button type='submit' id='subutton'><span>Submit</span></button>")
