a13126930e7040a7a6e2932adafb7613b881b3f6
John Armstrong <j2armstrong@gmail.com>
2017-09-14 12:10:13 -0400

    stuff

--- js/submission_form.js
+++ js/submission_form.js
@@ -208,7 +208,7 @@ $(document).ready(function() {
 
     $(".request_acct_input").last().append("<div class='request_acct_input_row'><div class='request_acct_table_row_label'></div></div>")
 
-    $(".request_acct_table_row_label").last().append('<label for="file-upload" class="file-upload">Upload Still<input id="file-upload" name="file-upload" type="file"/></label>')
+    $(".request_acct_table_row_label").last().append('<label for="file-upload" class="file-upload"> <input id="file-upload" name="file-upload" type="file"/> Upload Still</label>')
 
     $(".request_acct_input").last().append("<div class='request_acct_input_row'><div class='request_acct_table_row_label'></div></div>")
 
