c49b7a7742893f1b11942ca1e6050fc9177a749f
John Armstrong <j2armstrong@gmail.com>
2017-09-18 13:50:35 -0400

    thing

--- js/submission_form.js
+++ js/submission_form.js
@@ -222,7 +222,7 @@ $(document).ready(function() {
     $(".request_acct_input_large").last().append("<input tabindex=6 type='text' id='filmmakers_email' name='filmmakers_email'>")
     $(".request_acct_input_large").last().append("<div class='border_line'></div>")
 
-    $(".film_form_table").last().append("<div class='request_acct_row_row_spacer'></div>")
+    $(".film_form_table").last().append("<div class='request_acct_row_row_sm_spacer'></div>")
 
     // Last Name 
     $(".film_form_table").last().append("<div class='request_acct_row_row'></div>")
