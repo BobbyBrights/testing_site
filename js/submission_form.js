026236e45e31285fe5f69b630481f820707342f1
John Armstrong <j2armstrong@gmail.com>
2017-09-18 13:43:48 -0400

    thing

--- js/submission_form.js
+++ js/submission_form.js
@@ -205,10 +205,10 @@ $(document).ready(function() {
     $(".film_form_table").last().append("<div class='request_acct_row_row'></div>")
     $(".request_acct_row_row").last().append("<div class='request_acct_cell_left'></div>")
     $(".request_acct_cell_left").last().append("<div class='request_acct_label'></div>")
-    $(".request_acct_label").last().append("<div class='request_acct_label_text'><b>First Name</b></div>")
+    $(".request_acct_label").last().append("<div class='request_acct_label_text'><b>Filmmaker's First Name</b></div>")
     $(".request_acct_cell_left").last().append("<div class='request_acct_input'></div>")
     $(".request_acct_input").last().append("<div class='request_acct_input_large'></div>")
-    $(".request_acct_input_large").last().append("<input tabindex=1 type='text' id='firstname' name='firstname'>")
+    $(".request_acct_input_large").last().append("<input tabindex=1 type='text' id='filmmakers_firstname' name='filmmakers_firstname'>")
     $(".request_acct_input_large").last().append("<div class='border_line'></div>")
 
     $(".request_acct_row_row").last().append("<div class='request_acct_cell_spacer'></div>")
@@ -216,20 +216,20 @@ $(document).ready(function() {
     // Address
     $(".request_acct_row_row").last().append("<div class='request_acct_cell_right'></div>")
     $(".request_acct_cell_right").last().append("<div class='request_acct_label'></div>")
-    $(".request_acct_label").last().append("<div class='request_acct_label_text'><b>Address</b></div>")
+    $(".request_acct_label").last().append("<div class='request_acct_label_text'><b>Filmmaker's Email</b></div>")
     $(".request_acct_cell_right").last().append("<div class='request_acct_input'></div>")
     $(".request_acct_input").last().append("<div class='request_acct_input_large'></div>")
-    $(".request_acct_input_large").last().append("<input tabindex=6 type='text' id='address' name='address'>")
+    $(".request_acct_input_large").last().append("<input tabindex=6 type='text' id='filmmakers_email' name='filmmakers_email'>")
     $(".request_acct_input_large").last().append("<div class='border_line'></div>")
 
     // Last Name 
     $(".film_form_table").last().append("<div class='request_acct_row_row'></div>")
     $(".request_acct_row_row").last().append("<div class='request_acct_cell_left'></div>")
     $(".request_acct_cell_left").last().append("<div class='request_acct_label'></div>")
-    $(".request_acct_label").last().append("<div class='request_acct_label_text'><b>Last Name</b></div>")
+    $(".request_acct_label").last().append("<div class='request_acct_label_text'><b>Filmmaker's Last Name</b></div>")
     $(".request_acct_cell_left").last().append("<div class='request_acct_input'></div>")
     $(".request_acct_input").last().append("<div class='request_acct_input_large'></div>")
-    $(".request_acct_input_large").last().append("<input tabindex=2 type='text' id='lastname' name='lastname'>")
+    $(".request_acct_input_large").last().append("<input tabindex=2 type='text' id='filmmakers_lastname' name='filmmakers_lastname'>")
     $(".request_acct_input_large").last().append("<div class='border_line'></div>")
 
     //Screening history
