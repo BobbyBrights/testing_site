98dc0a4d9c7fc58e16cc54a1f9debab8c50284ca
John Armstrong <j2armstrong@gmail.com>
2017-09-18 15:01:22 -0400

    thing

--- js/submission_form.js
+++ js/submission_form.js
@@ -242,9 +242,9 @@ $(document).ready(function() {
     $(".request_acct_cell_left").last().append("<div class='request_acct_label'></div>")
     $(".request_acct_label").last().append("<div class='request_acct_label_text'><b>Filmmaker's Bio</b></div>")
     $(".request_acct_cell_left").last().append("<div class='request_acct_input'></div>")
-    $(".request_acct_input").last().append("<div class='request_acct_input_large'></div>")
-    $(".request_acct_input_large").last().append("<input type='textarea' id='filmmakers_bio' name='filmmakers_bio'>")
-    $(".request_acct_input_large").last().append("<div class='border_line'></div>")
+    $(".request_acct_input").last().append("<div class='request_acct_input_box'></div>")
+    $(".request_acct_input_box").last().append("<input type='textarea' id='filmmakers_bio' name='filmmakers_bio'>")
+    $(".request_acct_input_box").last().append("<div class='border_line'></div>")
 
     $(".film_form_table").last().append("<div class='request_acct_row_row_spacer'></div>")
 
