d8c8319efc9da857dca12a81c0dab1eb8ddd5f67
John Armstrong <j2armstrong@gmail.com>
2017-09-19 12:25:44 -0400

    thing

--- js/submission_form.js
+++ js/submission_form.js
@@ -453,6 +453,16 @@ $(document).ready(function() {
 
     $(".film_form_table").last().append("<div class='request_acct_row_row_spacer'></div>")
 
+    $(".film_form_table").last().append("<div class='note_table'></div>")
+    $(".note_table").last().append("<div class='note_left_spacer'></div>")
+    $(".note_table").last().append("<div class='note_content'></div>")
+    $(".note_content").last().append("<div class='note_content_table'></div>")
+    $(".note_content_table").last().append("<div class='note_content_horz_spacer'></div>")
+    $(".note_content_table").last().append("<div class='note_content_table_content'></div>")
+    $(".note_content_table_content").last().append("At this point they will submit film details here. Maybe some mention about copyrights etc.")
+    $(".note_content_table").last().append("<div class='note_content_horz_spacer'></div>")
+    $(".note_table").last().append("<div class='note_right_spacer'></div>")
+
     // Original Format
     $(".film_form_table").last().append("<div class='film_form_row_row'></div>")
     $(".film_form_row_row").last().append("<div class='selection_acct_label'></div>")
