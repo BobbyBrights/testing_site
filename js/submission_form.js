e9ec60a0c047f1a8a246bf6b07f0a7f56d02c3f7
John Armstrong <j2armstrong@gmail.com>
2017-09-19 13:21:14 -0400

    thing

--- js/submission_form.js
+++ js/submission_form.js
@@ -451,6 +451,8 @@ $(document).ready(function() {
     $(".acct_type").last().append("<label for='vimeo_link'></label>")
     $(".request_acct_film_row").last().append("<div class='request_acct_type_label_text'><b>vimeo password protected link</b></div>")
 
+    check_preview_format();
+
     // $(".film_form_table").last().append("<div class='request_acct_row_sm_spacer'></div>")
 
     // vimeo link
@@ -605,6 +607,12 @@ $(document).ready(function() {
     });
 
 })
+
+function check_preview_format() {
+    $("input[type=radio][name=preview_format]").change(function() {
+        alert($(this).val());
+    }) 
+}
 
 function check_screening_history_upload() {
 
