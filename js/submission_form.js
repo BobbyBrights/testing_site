4aea208bf1f54d0e507b8f862e9ce9c161214e3e
John Armstrong <j2armstrong@gmail.com>
2017-09-21 15:41:29 -0400

    thing

--- js/submission_form.js
+++ js/submission_form.js
@@ -372,7 +372,7 @@ $(document).ready(function() {
 
     $(".request_acct_row_row").last().append("<div class='request_acct_cell_spacer'></div>")
 
-    $(".film_form_table").last().append("<div class='request_acct_row_row_spacer'></div>")
+    $(".film_form_table").last().append("<div class='request_acct_row_row_sm_spacer'></div>")
 
     // Self Identification
 
