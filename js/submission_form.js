7f310bc303154f5dc1e6d051263a2f46e6d02e66
John Armstrong <j2armstrong@gmail.com>
2017-09-21 15:40:59 -0400

    thing

--- js/submission_form.js
+++ js/submission_form.js
@@ -387,7 +387,7 @@ $(document).ready(function() {
     $(".note_content_table").last().append("<div class='note_content_horz_spacer'></div>")
     $(".note_table").last().append("<div class='note_right_spacer'></div>")
 
-    $(".table_contents").append("<div class='spacer'></div>")
+    $(".film_form_table").last().append("<div class='request_acct_row_row_spacer'></div>")
 
     $(".film_form_table").last().append("<div class='request_acct_row_row'></div>")
     $(".request_acct_row_row").last().append("<div class='request_acct_cell_left'></div>")
