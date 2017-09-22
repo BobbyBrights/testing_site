cacebb35e7d2d4f5a1701e5ce9ee7ef98319dcc8
John Armstrong <j2armstrong@gmail.com>
2017-09-22 17:06:10 -0400

    thing

--- js/submission_form.js
+++ js/submission_form.js
@@ -248,7 +248,7 @@ $(document).ready(function() {
 
 function change_id_count(old_count, new_count) {
     $("#form_entry_" + old_count.toString() + " :input").each(function() {
-        console.log($(this).attr("name"));
+        console.log($(this).attr("id") + " " + $(this).attr("name"));
     })
 }
 
