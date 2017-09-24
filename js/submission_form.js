ae3e7b14d0132f5efea982b2abba0dde4d225c1c
John Armstrong <j2armstrong@gmail.com>
2017-09-24 16:10:43 -0400

    thing

--- js/submission_form.js
+++ js/submission_form.js
@@ -273,6 +273,8 @@ function change_id_count(old_count, new_count) {
 
     $("#form_entry_" + new_count.toString()).find('.subtitle').html("Film Entry #" + new_count.toString())
 
+    $("#vimeo_link_section_" + old_count.toString()).attr('id', "vimeo_link_section_" + new_count.toString())
+
     if ($("input[type=radio][name=preview_format_" + new_count.toString() + "]").val() !== "vimeo_link") {
         $("#vimeo_link_section_" + new_count.toString()).hide();
     }
