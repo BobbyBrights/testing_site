1e9d6ba60c57e69b493fa3b4a3e561d58bde6413
John Armstrong <j2armstrong@gmail.com>
2017-09-24 13:04:59 -0400

    thing

--- js/submission_form.js
+++ js/submission_form.js
@@ -684,7 +684,7 @@ function remove_film_form(button_obj) {
 
         $("#form_count").attr("value", (form_count - 1).toString())
 
-        $('#filmmaker_info_' + form_number).off('change');
+        $('#filmmaker_info_' + form_number).unbind('change');
 
         film_form_count--;
     });
