25796739a00314d0e1311dd925369c69e9ee686a
John Armstrong <j2armstrong@gmail.com>
2017-09-24 11:24:17 -0400

    thing

--- js/submission_form.js
+++ js/submission_form.js
@@ -796,7 +796,7 @@ function check_preview_format(old_count, new_count) {
 
 function check_filmmaker_section(old_count, new_count) {
     $('#filmmaker_info_' + old_count.toString()).off('change');
-    check_preview_format('#filmmaker_info_' + new_count.toString());
+    set_filmmaker_info($('#filmmaker_info_' + new_count.toString()));
 }
 
 function check_preview_format(entry_count) {
