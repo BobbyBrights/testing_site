6bef84f366af618f0d19b3b21d9d01830f25e4c3
John Armstrong <j2armstrong@gmail.com>
2017-09-26 17:11:41 -0400

    thing

--- js/submission_form.js
+++ js/submission_form.js
@@ -552,6 +552,11 @@ function make_film_entry(entry_count) {
 
     $(".film_form_table").last().append("<div class='request_acct_row_row_spacer'></div>")
 
+    $(".film_form_table").last().append("<div class='request_acct_row_row'></div>")
+    $(".request_acct_row_row").last().append("<div class='request_acct_cell_left'></div>")
+    $(".request_acct_cell_left").last().append("<div class='selection_acct_label_generic'></div>")
+    $(".selection_acct_label_generic").last().append("<div class='request_acct_label_text'>Please select as many formats as necessary: </div>")
+
     // Original Format
     $(".film_form_table").last().append("<div class='film_form_row_row'></div>")
     $(".film_form_row_row").last().append("<div class='selection_acct_label'></div>")
