bedd6f13eba5190c29cd7a918033353043f053ab
John Armstrong <j2armstrong@gmail.com>
2017-09-28 18:51:15 -0400

    thing

--- src/film_form_submit.php
+++ src/film_form_submit.php
@@ -11,8 +11,28 @@ else {
 	$client_id = -1;
 }
 
+// request info
+
 $firstname = $_POST['firstname'];
+$lastname = $_POST['lastname'];
+$address = $_POST['address'];
+$city_town = $_POST['city_town'];
+$country = $_POST['country_'];
+$association = $_POST['organization'];
+$province_state = $_POST['province_state'];
+$postal_code = $_POST['postal_code'];
+$phone = $_POST['phone'];
+$email = $_POST['email'];
 
 echo($firstname);
+echo($lastname);
+echo($address);
+echo($city_town);
+echo($country);
+echo($association);
+echo($province_state);
+echo($postal_code);
+echo($phone);
+echo($email);
 
 ?>
