f20e6164b499c81ef7cfb928c3a74dfd29d885ce
John Armstrong <j2armstrong@gmail.com>
2017-09-28 20:05:43 -0400

    thing

--- src/film_form_submit.php
+++ src/film_form_submit.php
@@ -30,7 +30,7 @@ $number_of_forms = $_POST['form_count'];
 
 // print_r($_POST['search-org-format_1']);
 
-echo(implode(',', $_POST['search-org-format_1']))
+echo(implode(',', $_POST['search-org-format_1']));
 
 $outcome = 0;
 
