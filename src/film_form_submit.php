a8cd169ddbccbd2332dbca5af85ef7360fee44df
John Armstrong <j2armstrong@gmail.com>
2017-09-28 21:23:27 -0400

    thing

--- src/film_form_submit.php
+++ src/film_form_submit.php
@@ -3,9 +3,11 @@
 define ('SITE_ROOT', realpath(dirname(getcwd())));
 
 $target_dir = SITE_ROOT . "/upload/";
-$target_file = $target_dir . guidv4(openssl_random_pseudo_bytes(16)) . ".jpg";
+// $target_file = $target_dir . guidv4(openssl_random_pseudo_bytes(16)) . ".jpg";
 
-move_uploaded_file($_FILES["film-still-file_1"]["tmp_name"], $target_file);
+// move_uploaded_file($_FILES["film-still-file_1"]["tmp_name"], $target_file);
+
+echo $target_dir;
 
 if(session_status()==1) {
     @session_start();
