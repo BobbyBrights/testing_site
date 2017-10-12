5d62da599f4517005ae0c0a2877c19cf83ed19ca
John Armstrong <j2armstrong@gmail.com>
2017-10-12 11:57:54 -0400

    thing

--- js/toolbar.js
+++ js/toolbar.js
@@ -103,6 +103,14 @@ var toolbar_contents = '\
 			<img class="logo" src="' + web_host + 'img/toolbar/' + logo_ims[randomnumber] + '">\
 			</a>\
 			<div class="right_links">\
+			<div class="twitter_logo">\
+				<a href="https://twitter.com/cfmdc/" target="_blank" title="Twitter"><i class="fa fa-twitter" aria-hidden="true"></i></a>\
+			</div>\
+			<div class="spacer"></div>\
+			<div class="instagram_logo">\
+				<a href="https://www.instagram.com/cfmdcfilm/" target="_blank" title="Instagram"><i class="fa fa-instagram" aria-hidden="true"></i></a>\
+			</div>\
+			<div class="spacer"></div>\
 							<div class="facebook_logo">\
 				<a href="https://www.facebook.com/cfmdcfilm/" target="_blank" title="Facebook"><i class="fa fa-facebook-official" aria-hidden="true"></i></a>\
 			</div>\
