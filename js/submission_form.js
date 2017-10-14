c9b9d4eba0fb94e85e17388592b4a688adf303de
John Armstrong <j2armstrong@gmail.com>
2017-10-14 15:07:56 -0400

    thing

--- js/submission_form.js
+++ js/submission_form.js
@@ -681,7 +681,7 @@ function make_film_entry(entry_count) {
     $(".note_content_table").last().append("<div class='note_content_horz_spacer'></div>")
     $(".note_content_table").last().append("<div class='note_content_table_content'></div>")
     $(".note_content_table_content").last().append("<div class='please_list'><div id='please_list_heading_" + entry_count.toString() +"'><b>Please list:</b></div><br/></div>")
-    $(".please_list").last().append("<div class='note_content_horz_spacer'></div><input type='text' id='distribution_list_" + entry_count.toString() + "' name='distribution_list_" + entry_count.toString() + "'/><div class='note_content_horz_spacer'></div>")
+    $(".please_list").last().append("<input type='text' id='distribution_list_" + entry_count.toString() + "' name='distribution_list_" + entry_count.toString() + "'/><div class='note_content_horz_spacer'></div>")
     $(".please_list").last().hide()
     $(".note_table").last().append("<div class='note_right_spacer'></div>") 
 
