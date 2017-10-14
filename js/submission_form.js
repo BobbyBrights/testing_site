f28d47cc287b491e41fd670ef89fc505f54b67cf
John Armstrong <j2armstrong@gmail.com>
2017-10-14 13:31:07 -0400

    thing

--- js/submission_form.js
+++ js/submission_form.js
@@ -675,6 +675,9 @@ function make_film_entry(entry_count) {
     $(".note_content_table_content").last().append("<div><div class='distribution'><input type='radio' value='yes' id='yes_distri_" + entry_count.toString() +"' name='distri_" + entry_count.toString() +"'/><label for='yes_distri_" + entry_count.toString() + "'></label></div>&nbsp;<b>Yes</b></div>")
     $(".note_content_table_content").last().append("<div><div class='distribution'><input type='radio' value='no' id='no_distri_" + entry_count.toString() +"' name='distri_" + entry_count.toString() +"'/><label for='no_distri_" + entry_count.toString() + "'></label></div>&nbsp;<b>No</b></div>")
     $(".note_content_table").last().append("<div class='note_content_horz_spacer'></div>")
+    $(".note_content_table").last().append("<div class='note_content_table_content'></div>")
+    $(".note_content_table_content").last().append("<b>Please list:</b><br/>")
+    $(".note_content_table").last().append("<div class='note_content_horz_spacer'></div>")
     $(".note_table").last().append("<div class='note_right_spacer'></div>") 
 
     $(".film_form_table").last().append("<div class='request_acct_row_row_spacer'></div>")
