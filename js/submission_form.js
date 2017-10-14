1b8b38ad2678cbef0e03f914e796e6a3b3fffe3e
John Armstrong <j2armstrong@gmail.com>
2017-10-14 12:55:34 -0400

    thing

--- js/submission_form.js
+++ js/submission_form.js
@@ -670,7 +670,7 @@ function make_film_entry(entry_count) {
     $(".note_content_table").last().append("<div class='note_content_horz_spacer'></div>")
     $(".note_content_table").last().append("<div class='note_content_table_content'></div>")
     $(".note_content_table_content").last().append("<b>Is your title in distribution with any other organizations?</b><br/>")
-    $(".note_content_table_content").last().append("<div class='ditrib'><input type='radio' value='yes' id='yes_distri_" + entry_count.toString() +"' name='distri_" + entry_count.toString() +"'/><label for='yes_distri_" + entry_count.toString() + "'>Yes</label></div>")
+    $(".note_content_table_content").last().append("<div class='ditrib'><input type='radio' value='yes' id='yes_distri_" + entry_count.toString() +"' name='distri_" + entry_count.toString() +"'/><label for='yes_distri_" + entry_count.toString() + "'></label></div>")
     $(".note_content_table").last().append("<div class='note_content_horz_spacer'></div>")
     $(".note_table").last().append("<div class='note_right_spacer'></div>") 
 
